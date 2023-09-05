3eb9d14180f0025a8588b66aeb4a294cda85c658
e1ysium <43900243+deltaRing@users.noreply.github.com>
2023-09-05 13:51:06 +0800

    对Dstar进行了小幅度修改

--- define/Dstar/Dstar_algorithm/Dstar_realtime.m
+++ define/Dstar/Dstar_algorithm/Dstar_realtime.m
@@ -13,7 +13,16 @@
 % 输出1：路径                path
 % 输出2：每个路径的期望方位角   azis
 % 输出3：每个路径的期望俯仰角   eles
-function [path, azis, eles] = Dstar_realtime(map, ...
+
+% 2023年 8月 31日 新增输出
+% QRecord：记录Dstar路径
+% QRecordEnd：记录Dstar结束的路径
+% QoptRecord：记录最佳路径
+% ScannerRecord： 记录第几个迭代的情况下所扫描到的结果
+
+function [path, azis, eles, QRecord, ...
+    QRecordEnd, QoptRecord, ScannerRecord] = ...
+    Dstar_realtime(map, ...
     start_state, end_state, ...
     azimuth, elevation, ...
     building, ...
@@ -31,6 +40,13 @@ function [path, azis, eles] = Dstar_realtime(map, ...
     azis = [];
     eles = [];
 
+    % 初始化绘制
+    iter          = 0;
+    QRecord       = {};
+    QoptRecord    = {};
+    ScannerRecord = {};
+    QRecordEnd    = [];
+    
     figure(10004)
     % 绘制建筑物
     for ii = 1:size(building, 1)
@@ -56,6 +72,9 @@ function [path, azis, eles] = Dstar_realtime(map, ...
     middle_state = start_state;
     
     while ~checkNode(middle_state, end_state)
+        % 迭代次数自增
+        iter = iter + 1;
+      
         current_location = [middle_state.x, ...
             middle_state.y ...
             middle_state.z];
@@ -92,6 +111,54 @@ function [path, azis, eles] = Dstar_realtime(map, ...
         middle_state = Dstar.states(middle_state.x, ...
                                     middle_state.y, ...
                                     middle_state.z);
+        
+        % 找到路径
+        Path = ExtractPath(middle_state);
+        % 找到路径上障碍物
+        Q      = b_spline_generate(Path);
+        [P, V] = FindObstacleVectorDstar(Path(2:end,:), map_observed);
+        
+        CostFunc = @(Q_)compute_cost(Q_, P, V);
+        [Popt, Cost] = fminsearch(CostFunc, Path(2:end,:));
+        disp(Cost)
+        Qopt = b_spline_generate([Path(1,:); Popt]);
+        
+        QRecord{iter}       = {Path};
+        QoptRecord{iter}    = {Qopt};
+        ScannerRecord{iter} = {map_observed};
+        end_iter      = 0; % 记录结束本次迭代的次数
+        
+        if ~isempty(Q) && ~isempty(Q)
+            figure(10005)
+            hold off
+             % 绘制建筑物
+            for ii = 1:size(building, 1)
+                plot_building(building(ii,:));
+            end
+            plot3(Q(:,1), Q(:,2), Q(:,3))
+            hold on
+            scatter3(Path(:,1), Path(:,2), Path(:,3),5, 'p', 'filled')
+            plot3(Qopt(:,1), Qopt(:,2), Qopt(:,3))
+            scatter3(Popt(:,1), Popt(:,2), Popt(:,3),5, 'g', 'filled')
+
+            % 显示障碍物
+            xxxx = []; yyyy = []; zzzz = [];
+            for xxx = 1:size(map_observed, 1)
+                for yyy = 1:size(map_observed, 2)
+                    for zzz = 1:size(map_observed, 3)
+                        if map_observed(xxx, yyy, zzz) == obstacle
+                            xxxx = [xxxx; xxx];
+                            yyyy = [yyyy; yyy];
+                            zzzz = [zzzz; zzz];
+                        end
+                    end
+                end
+            end
+            scatter3(xxxx, yyyy, zzzz, 5, 'r', 'filled') 
+            % 绘制路径点
+        end
+        
+        figure(10004)
         % 检测次数
         detect_time = 0;               
         % 运行数据
@@ -99,6 +166,7 @@ function [path, azis, eles] = Dstar_realtime(map, ...
                 middle_state.state ~= unknown
             middle_state_p = middle_state.parent; % 
             
+            end_iter = end_iter + 1;
             %
             % 得到角度
             %
@@ -124,7 +192,7 @@ function [path, azis, eles] = Dstar_realtime(map, ...
                 break;
             end
             
-            % 记录位置
+            % 2. 记录位置
             path = [path; current_location];
             azis = [azis; azimuth];
             eles = [eles; elevation];
@@ -170,44 +238,8 @@ function [path, azis, eles] = Dstar_realtime(map, ...
         end
         % 绘制路径
         plot_Dstar_path(Dstar, current_location)
-        
-         % 找到路径
-        Path = ExtractPath(middle_state);
-        % 找到路径上障碍物
-        Q      = b_spline_generate(Path);
-        [P, V] = FindObstacleVectorDstar(Path, map_observed);
-        
-        CostFunc = @(Q_)compute_cost(Q_, P, V);
-        [Popt, Cost] = fminsearch(CostFunc, Path);
-        disp(Cost)
-        Qopt = b_spline_generate(Popt);
-        
-        figure(10005)
-        hold off
-         % 绘制建筑物
-        for ii = 1:size(building, 1)
-            plot_building(building(ii,:));
-        end
-        plot3(Q(:,1), Q(:,2), Q(:,3))
-        hold on
-        scatter3(Path(:,1), Path(:,2), Path(:,3),5, 'p', 'filled')
-        plot3(Qopt(:,1), Qopt(:,2), Qopt(:,3))
-        scatter3(Popt(:,1), Popt(:,2), Popt(:,3),5, 'g', 'filled')
-        
-        % 显示障碍物
-        xxxx = []; yyyy = []; zzzz = [];
-        for xxx = 1:size(map_observed, 1)
-            for yyy = 1:size(map_observed, 2)
-                for zzz = 1:size(map_observed, 3)
-                    if map_observed(xxx, yyy, zzz) == obstacle
-                        xxxx = [xxxx; xxx];
-                        yyyy = [yyyy; yyy];
-                        zzzz = [zzzz; zzz];
-                    end
-                end
-            end
-        end
-        scatter3(xxxx, yyyy, zzzz, 5, 'r', 'filled') 
+        % 
+        QRecordEnd = [QRecordEnd end_iter];
     end
     
 end
